8b32d966d26335696e8281bffe833c287f9f549d
dotnetetc <contactus@dotnetetc.com>
2018-09-15 22:45:55 +0530

    Check in Send Email using C#

---
+++ GmailDemo/GEmail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace GmailDemo
+{
+    public class GEmail
+    {
+        public MailAddress SenderEmail { get; set; }
+        public MailAddress RecipientEmail { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string GmailId { get; set; }
+        public string GmailPwd { get; set; }
+
+        public static void SendMessage(GEmail gm)
+        {
+            MailMessage gmail = new MailMessage(gm.SenderEmail, gm.RecipientEmail);
+            SmtpClient client = new SmtpClient();
+            client.Port = 25;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(gm.GmailId, gm.GmailPwd);
+
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            gmail.Subject = gm.Subject;
+            gmail.Body = gm.Message;
+            client.Send(gmail);
+        }
+    }
+}
---
+++ GmailDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GmailDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GEmail gmail = new GEmail();
+            Console.WriteLine("Enter Sender Email");
+            gmail.SenderEmail = new MailAddress(Console.ReadLine());
+            Console.WriteLine("Enter Recipient Email");
+            gmail.RecipientEmail = new MailAddress(Console.ReadLine());
+            Console.WriteLine("Enter Subject");
+            gmail.Subject = Console.ReadLine();
+            Console.WriteLine("Enter email message");
+            gmail.Message = Console.ReadLine();
+            Console.WriteLine("Enter gmail id");
+            gmail.GmailId = Console.ReadLine();
+            Console.WriteLine("Enter password");
+            gmail.GmailPwd = Console.ReadLine();
+
+            try
+            {
+                GEmail.SendMessage(gmail);
+                Console.WriteLine("Mail send");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occured "+ex.Message);
+                Console.ReadLine();
+            }
+           
+
+        }
+    }
+
+  
+}
